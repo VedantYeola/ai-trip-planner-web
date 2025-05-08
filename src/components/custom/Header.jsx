5bba332d36a7510e966f578a207b0bef5ab1e893
VedantYeola <vedantyeola16@gmail.com>
2025-05-08 15:52:32 +0530

    Initial updatet

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -4,7 +4,7 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
 import { googleLogout, useGoogleLogin } from "@react-oauth/google";
 import { FcGoogle } from "react-icons/fc";
 import {
@@ -15,7 +15,6 @@ import {
 } from "@/components/ui/dialog";
 import axios from "axios";
 
-
 function Header() {
   const user = JSON.parse(localStorage.getItem("user"));
   const [openDialog, setOpenDialog] = useState(false);
@@ -25,11 +24,11 @@ function Header() {
 
   const login = useGoogleLogin({
     onSuccess: (tokenResponse) => GetUserProfile(tokenResponse),
-    onError: (error) => console.log(error)
+    onError: (error) => console.log(error),
   });
 
   const GetUserProfile = (tokenInfo) => {
-    axios 
+    axios
       .get(
         `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo.access_token}`,
         {
@@ -46,40 +45,57 @@ function Header() {
       });
   };
 
-
   return (
     <div className="p-3 shadow-md flex justify-between items-center px-5">
-      <img src="/logot-.png" alt="Travel Trip" className="h-20 w-auto object-contain " />
+      <img
+        src="/logot-.png"
+        alt="Travel Trip"
+        className="h-20 w-auto object-contain "
+      />
 
-      <div>{user ? 
-        <div className="flex items-center gap-3">
-           <a href="/create-trip">
-        <Button variant="outline" className=" text-black rounded-full">+ Create Trip</Button>
-        </a>
-          <a href="/my-trips">
-        <Button variant="outline" className=" text-black rounded-full">My Trips</Button>
-        </a>
-        <Popover>
-        <PopoverTrigger>
-        <img src={user?.picture}className="h-[35px] w-[35px] rounded-full"/>
-        </PopoverTrigger>
-        <PopoverContent>
-          <h2 className="cursor-pointer" onClick={()=>{
-            googleLogout();
-            localStorage.clear();
-            window.location.reload();
-          }}>Logout</h2>
-          </PopoverContent>
-        </Popover>
-
-      </div> : <Button onClick={()=>setOpenDialog(true)}>Sign In</Button>
-      }
+      <div>
+        {user ? (
+          <div className="flex items-center gap-3">
+            <a href="/create-trip">
+              <Button variant="outline" className=" text-black rounded-full">
+                + Create Trip
+              </Button>
+            </a>
+            <a href="/my-trips">
+              <Button variant="outline" className=" text-black rounded-full">
+                My Trips
+              </Button>
+            </a>
+            <Popover>
+              <PopoverTrigger>
+                <img
+                  src={user?.picture}
+                  className="h-[35px] w-[35px] rounded-full"
+                />
+              </PopoverTrigger>
+              <PopoverContent>
+                <h2
+                  className="cursor-pointer"
+                  onClick={() => {
+                    googleLogout();
+                    localStorage.clear();
+                    window.location.reload();
+                  }}
+                >
+                  Logout
+                </h2>
+              </PopoverContent>
+            </Popover>
+          </div>
+        ) : (
+          <Button onClick={() => setOpenDialog(true)}>Sign In</Button>
+        )}
       </div>
       <Dialog open={openDialog}>
         <DialogContent>
           <DialogHeader>
             <DialogDescription>
-              <img src="logo.svg" />
+              <img src="/logot-.png" />
               <h2 className="font-bold text-lg mt-7">Sign In With Google</h2>
               <p>Sign in to the App with Google authentication securely</p>
 
@@ -93,7 +109,7 @@ function Header() {
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
-      </Dialog>      
+      </Dialog>
     </div>
   );
 }
